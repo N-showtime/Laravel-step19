19ab8ec2238adf32ece2fc61663f2e6420f93679
Shogo.N <hozukl.1225@gmail.com>
2025-10-12 10:38:32 +0900

    ミドルウェアを活用し、管理者のみをアクセス可能とする処理を実装

--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -35,12 +35,15 @@ Route::middleware(['auth'])->group(function () {
         ->name('two-factor.show');
 });
 
-// postの作成
-Route::get('post/create', [PostController::class, 'create']);
-Route::post('post', [PostController::class, 'store'])->name('post.store');
+Route::middleware(['auth', 'admin'])->group(function ()
+{
+    // postの作成
+    Route::get('post/create', [PostController::class, 'create']);
+    Route::post('post', [PostController::class, 'store'])->name('post.store');
 
-// 一覧画面
-Route::get('post', [PostController::class, 'index']);
+    // 一覧画面
+    Route::get('post', [PostController::class, 'index']);
+});
 
 
 require __DIR__.'/auth.php';
