ff8e1406d253c1c6e314b10a39fa1c39faff56cd
Shogo.N <hozukl.1225@gmail.com>
2025-11-02 13:20:36 +0900

    ２段階認証を実装

--- myproject/app/Models/User.php
+++ myproject/app/Models/User.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use Notifiable;
+}
 
 class User extends Authenticatable
 {
